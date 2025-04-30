3f7d82b38a88f04eaaae1454706aaec20b04b178
hyejin <95wlalsxogud@gmail.com>
2025-04-30 14:41:32 +0900

    feat: 소비자로그인 에러핸들링 수정

--- src/querys/ConsumerQuerys.ts
+++ src/querys/ConsumerQuerys.ts
@@ -27,8 +27,21 @@ export const useConsumerLogin = () => {
             }
         },
         onError: (error: any) => {
+            // error.status
+            // 404: 이메일이 없는 경우
+            // 409: 비밀번호가 맞지 않는 경우
+            let errorMessage = '';
+
+            if (error.status === 404) {
+                errorMessage = 'Please check your email.';
+            } else if (error.status === 409) {
+                errorMessage = 'Please check your password.';
+            } else {
+                errorMessage = error.message || 'An unexpected error occurred.';
+            }
+
             modal.open({
-                message: `${error.message}`,
+                message: errorMessage,
                 onConfirm: () => modal.close(),
                 hasCancel: false,
                 confirmButtonContent: { children: '확인' }
